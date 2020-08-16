964a7ab5466d13d6c9a80f77fc706f491691e625
youmekko <youmekko.o@gmail.com>
2020-08-16 17:25:22 +0900

    feat : 리덕스 스토어에 devTools extentsion 등 미들웨어 추가

--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -1,11 +1,14 @@
 import { createWrapper } from 'next-redux-wrapper'
-
 import reducer from '../reducers'
-
-import { createStore } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 const configureStroe = () => {
-    const store = createStore(reducer)
+    const middlewares = []
+    const enhancer = process.env.NODE_ENV === 'production'
+    ? compose(applyMiddleware(...middlewares))
+    : composeWithDevTools(applyMiddleware(...middlewares))
+    const store = createStore(reducer, enhancer)
     return store
 }
 
